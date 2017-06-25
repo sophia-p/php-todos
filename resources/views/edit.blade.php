c6200f1b599389fc6cbc172c6ea7217268c2642d
Sophia Pignatelli <pignatelli@fordham.edu>
2017-06-25 00:20:44 -0400

    add edit task template

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -2,4 +2,19 @@
 
 @section('content')
 
-@endsection
+	<div class="container">
+		<h1>Edit task</h1>
+
+		<form method="POST" action="/task/{{ $task->id }}">
+			<div class="form-group">
+				<textarea name="description" class="form-control">{{$task->description}}</textarea>
+			</div>
+
+			<div class="form-group">
+				<button type="submit" name="update" class="btn btn-primary">Update task</button>
+			</div>
+		{{ csrf_field() }}
+		</form>
+	</div>
+
+@stop
