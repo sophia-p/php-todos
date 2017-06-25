ee208dbc052e07668a7cfa9d00ac9676d1622e7d
Sophia Pignatelli <pignatelli@fordham.edu>
2017-06-25 00:14:47 -0400

    add task template

--- resources/views/add.blade.php
+++ resources/views/add.blade.php
@@ -1,5 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="container">
+	<h2>Add New Task</h2>
+
+	<form method="POST" action="/task">
+		<div class="form-group">
+			<textarea name="description" class="form-control">
+		</div>
+
+		<div class="form-group">
+			<button type="submit" class="btn btn-primary">Add task</button>
+		</div>
+	{{ csrf_field() }}
+	</form>
+</div>
 
 @endsection
